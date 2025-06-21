bc63bd5d64f652441177db43303c4f2e93253f0b
Oscar Del Pino Mercado <oscardpm18@hotmail.com>
2025-06-21 13:44:34 -0600

    fixing issues with the user_login app, added urls.py file that was lost during prev merge, adding forms.py file that was also lost during prev merge

---
+++ user_login/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = 'user_login'
+
+urlpatterns = [
+    path("", CustomLoginView.as_view(), name="login"),
+    path("index/", index, name="index"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("profile/", profile, name="profile"),
+    path("signup/", CustomSignupView.as_view(), name="signup"),
+    path("password_change/", CustomPasswordChangeView.as_view(), name="password_change"),
+]
